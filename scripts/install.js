b1dc7cfa58beb0b22492f4dc501115fefdda8fc3
Thilina <thilina@aligent.com.au>
2019-02-04 07:37:18 +1030

    init 2

--- scripts/install.js
+++ scripts/install.js
@@ -7,4 +7,110 @@ const cp = require('child_process');
 const emoji = require('node-emoji');
 const { Select, Input } = require('enquirer');
 
+const getExistingThemes = require('./get-existing-themes');
 const logger = require('./logger');
+const { baseDir, scssSource } = require('./constants');
+
+checkEnvironment(); // Ensure the script is being called from the right place
+
+const themesOptions = getExistingThemes();
+
+const selectTheme = new Select({
+    name: 'theme',
+    message: 'Select theme name',
+    choices: [
+        ...themesOptions,
+        'Other'
+    ]
+});
+
+/**
+ * Check that the user is running the install command in the right directory
+ *
+ * @returns {Boolean}
+ */
+function checkEnvironment() {
+    if (!fs.existsSync(baseDir)) {
+        logger('Please run from base of Magento 2 project', 'error');
+        process.exit();
+    }
+}
+
+/**
+ * Get the path to the theme that the repository is being installed in to
+ *
+ * @returns {string}
+ */
+async function getThemeDirectory() {
+    let selectedTheme = 'Other'; // Default to 'Other' so that the user is asked to enter a theme name if none already exist
+
+    // If there are existing themes, allow the user to optionally select one
+    if (themesOptions.length > 0) {
+        selectedTheme = await selectTheme.run();
+    }
+
+    if (selectedTheme === 'Other') {
+        const createNewTheme = new Input({
+            message: 'Please enter new theme name in format [Vendor]/[Theme]'
+        });
+
+        selectedTheme = await createNewTheme.run();
+    }
+
+    logger('Theme directory selected', 'log');
+    logger(`New theme will be created in ${baseDir}${selectedTheme}`, 'log');
+    return `${baseDir}${selectedTheme}`;
+}
+
+/**
+ * Get the source directory for SCSS files
+ *
+ * @param {string} themeDirectory The base theme directory that we're working from
+ *
+ * @returns {string}
+ */
+function getSCSSSourceDirectory (themeDirectory) {
+    return `${themeDirectory}/${scssSource}`;
+}
+
+function downloadInstallRepository(themeDirectory) {
+    logger('Downloading Aligent SCSS Outline repository', 'log');
+    // Download a zip of the archive
+    cp.execSync('git archive --remote=ssh://git@bitbucket.org/aligent/scssoutline.git --format=zip --output="scssoutline.zip" master');
+    logger('Aligent SCSS Outline successfully downloaded', 'success');
+    cp.execSync(`unzip scssoutline.zip -d '${themeDirectory}/${scssSource}'`); // Extract to the SCSS source directory
+    cp.execSync('rm scssoutline.zip'); // Remove the downloaded zip
+    // Remove all files/folder that aren't the `src` directory
+    cp.execSync(`find ${themeDirectory}/${scssSource} -mindepth 1 ! -regex \'^${themeDirectory}/${scssSource}/src\\(/.*\\)?\' -delete`);
+    cp.execSync(`mv ${themeDirectory}/${scssSource}/src/* ${themeDirectory}/${scssSource}`); // Move everything in the `src` directory up one level, to be in the root of the SCSS source directory
+    cp.execSync(`rm -rf ${themeDirectory}/${scssSource}/src`); // Remove the now empty `src` directory
+    logger('Aligent SCSS Outline successfully installed', 'success');
+}
+
+/**
+ * Install the Aligent SCSS Placeholder repository, which is required for the Aligent SCSS Outline to compile
+ *
+ * @returns {Buffer}
+ */
+function installDependency() {
+    logger('Installing dependencies', 'log');
+    return cp.execSync('npm i -S git+ssh://git@bitbucket.org/aligent/scssplaceholder.git#master');
+}
+
+try {
+    (async () => {
+        const themeDirectory = await getThemeDirectory();
+        const scssSource = getSCSSSourceDirectory(themeDirectory);
+        logger('Checking for SCSS source directory', 'log');
+        fs.ensureDirSync(scssSource); // Checks that there is a SCSS source directory, and creates it if not
+        downloadInstallRepository(themeDirectory);
+        installDependency();
+        logger('Dependencies installed', 'success');
+        logger(`Now is a good time to create a pull request ${emoji.get('fire')}`, 'log');
+    })();
+
+} catch (err) {
+    logger('There was an error installing the package', 'error');
+}
+
+
---
+++ src/requirejs-config.js
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+var config = {
+    map: {
+        '*': {
+            // package: 'js/path/to/Package'
+        }
+    },
+    config: {
+        mixins: {
+        }
+    }
+};
